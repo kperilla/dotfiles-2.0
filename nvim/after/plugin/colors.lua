5cf773be18639ce45474fc86f1ab6cd77f1acae8
kperilla <kevin.m.perilla@gmail.com>
2023-12-23 16:44:37 +0000

    Add some colors

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,12 @@
 function ColorMyPencils(color)
-	color = color or "kanagawa"
-	vim.cmd.colorscheme(color)
+    -- Colors:
+    -- kanagawa
+    -- catppuccin-mocha
+    color = color or "catppuccin-mocha"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "#1b1421" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1b2421" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#1b1421" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1b2421" })
 end
 
 ColorMyPencils()
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,38 +1,44 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-   'rebelot/kanagawa.nvim',
-   {
-     'nvim-telescope/telescope.nvim', tag = '0.1.5',
- -- or                              , branch = '0.1.x',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-   },
-   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-   'tpope/vim-fugitive',
-   'rstacruz/vim-closer',
-   -- lsp-zero
- --  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
- --  {'neovim/nvim-lspconfig'},
- --  {'hrsh7th/cmp-nvim-lsp'},
- --  {'hrsh7th/nvim-cmp'},
- --  {'L3MON4D3/LuaSnip'},
-   -- lsp-zero
-   'williamboman/mason.nvim',
-   'williamboman/mason-lspconfig.nvim',
-   'neovim/nvim-lspconfig',
-   -- 'folke/trouble.nvim',
+    'rebelot/kanagawa.nvim',
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    'tpope/vim-fugitive',
+    'rstacruz/vim-closer',
+    -- lsp-zero
+    --  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    --  {'neovim/nvim-lspconfig'},
+    --  {'hrsh7th/cmp-nvim-lsp'},
+    --  {'hrsh7th/nvim-cmp'},
+    --  {'L3MON4D3/LuaSnip'},
+    -- lsp-zero
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+    -- 'folke/trouble.nvim',
+    {
+        'm4xshen/hardtime.nvim',
+        dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+        opts = {}
+    },
+    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 }
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
-
