2fb0fe081b0fa97313088bc98d695c82f0dcd9fd
kperilla <kevin.m.perilla@gmail.com>
2023-12-24 15:56:20 -0600

    Add Comment.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -40,7 +40,8 @@ local plugins = {
 --        dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
 --        opts = {}
 --    },
-    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 }
+    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+    'numToStr/Comment.nvim',
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
