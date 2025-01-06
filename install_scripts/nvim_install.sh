d2ff7f81dd0f36e6a0df3f621acb1d049dbff207
kperilla <kloiselperilla@gmail.com>
2025-01-06 17:04:40 -0500

    Add nvim install script

---
+++ install_scripts/nvim_install.sh
@@ -0,0 +1,13 @@
+CONF_DIR=$HOME/.config
+
+echo "Backing up old Neovim configurations to $CONF_DIR/backups..."
+if [ -d $CONF_DIR/nvim ]; then
+    cp -r $CONF_DIR/nvim $CONF_DIR/backups
+    rm -r $CONF_DIR/nvim
+fi
+if [ ! -f ./nvim/lua/kevin/my_lsp_servers.lua ]; then
+    cp ./nvim/lua/kevin/my_lsp_servers.lua.template ./nvim/lua/kevin/my_lsp_servers.lua
+fi
+
+echo "Installing Neovim config files..."
+cp -r ./nvim $CONF_DIR/nvim
