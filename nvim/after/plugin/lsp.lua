6ae886a5cdc423d2218de6a012a45e61760fcc11
kperilla <kloiselperilla@gmail.com>
2025-01-08 13:17:14 -0500

    Get lua_ls to recognize vim

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -83,4 +83,33 @@ require('mason-lspconfig').setup_handlers({
             capabilities = lsp_capabilities,
         })
     end,
+    lua_ls = function()
+        require("lspconfig").lua_ls.setup({
+            settings = {
+                Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = "LuaJIT"
+                    },
+                    diagnostics = {
+                        globals = { "vim" }
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                            -- Depending on the usage, you might want to add additional paths here.
+                            -- E.g.: For using `vim.*` functions, add vim.env.VIMRUNTIME/lua.
+                            -- "${3rd}/luv/library"
+                            -- "${3rd}/busted/library",
+                            vim.env.VIMRUNTIME .. "/lua"
+                        }
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    }
+                }
+            }
+        })
+    end,
 })
